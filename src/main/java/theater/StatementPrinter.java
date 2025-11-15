b82a1f4c2996e73cec88ee8ff570d13627b4e5e4
Victor Polowy <victor.polowy@mail.utoronto.ca>
2025-11-14 19:01:43 -0500

    Task 2.1 - Inline Variable thisamount

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -33,8 +33,6 @@ public class StatementPrinter {
 
         for (Performance p : invoice.getPerformances()) {
 
-            final int amount = getAmount(p);
-
             // add volume credits
             volumeCredits += Math.max(p.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
             // add extra credit for every five comedy attendees
@@ -45,8 +43,8 @@ public class StatementPrinter {
 
             }
             result.append(String.format("  %s: %s (%s seats)%n", getPlay(p).getName(),
-                    frmt.format(amount / Constants.PERCENT_FACTOR), p.getAudience()));
-            totalAmount += amount;
+                    frmt.format(getAmount(p) / Constants.PERCENT_FACTOR), p.getAudience()));
+            totalAmount += getAmount(p);
         }
         result.append(String.format("Amount owed is %s%n", frmt.format(totalAmount / Constants.PERCENT_FACTOR)));
         result.append(String.format("You earned %s credits%n", volumeCredits));
